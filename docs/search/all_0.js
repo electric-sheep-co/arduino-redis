01d83fdeb6a29449050335b19391b32d226e6ae0
Ryan Joseph <ryan.joseph@gmail.com>
2019-02-07 21:15:09 -0800

    Doc update

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['append',['append',['../classRedis.html#aff7468b0f29b883576227a60b86a2a2d',1,'Redis']]],
   ['authenticate',['authenticate',['../classRedis.html#a8f9a19a438e2f005852dcc7cd46816e5',1,'Redis']]],
   ['arduino_2dredis',['Arduino-Redis',['../index.html',1,'']]],
   ['arduino_20redis_20library',['Arduino Redis Library',['../md_README.html',1,'']]]
--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['exists',['exists',['../classRedis.html#a5acf5d9debf30048fcca5f4ad19e74f5',1,'Redis']]],
   ['expire',['expire',['../classRedis.html#a821419ef0401582307d7c26a4764cdf8',1,'Redis']]],
   ['expire_5fat',['expire_at',['../classRedis.html#a3caae04f0c00c019ddd4cba6be6355d2',1,'Redis']]],
   ['examples',['Examples',['../md_examples_README.html',1,'']]]
