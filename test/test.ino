6988949cf67ba53d6df0828dd9cfa97d1f8fd140
Ryan Joseph <ryan@proxy.com>
2021-01-10 13:25:41 -0800

    wip

--- test/test.ino
+++ test/test.ino
@@ -15,7 +15,9 @@ TestResults runTests(Redis *redis, String prefix, bool retainData = false);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Connecting...");
+  delay(2000);
+  Serial.printf("\n\n\n");
+  Serial.println("Arduino-Redis tests starting...");
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
 
@@ -23,7 +25,7 @@ void setup() {
     delay(1000);
   }
 
-  Serial.print("Connected! IP is: ");
+  Serial.print("Connected to network, IP is: ");
   Serial.println(WiFi.localIP());
   
   WiFiClient rc;
@@ -35,10 +37,13 @@ void setup() {
   auto r = new Redis(rc);
   if (!strlen(REDIS_AUTH) || r->authenticate(REDIS_AUTH) == RedisSuccess) {
     Serial.printf("Connection is%s authenticated\n", strlen(REDIS_AUTH) ? "" : " NOT");
+    
     randomSeed(analogRead(0));
     auto keyPrefix = String("__arduino_redis__test") + ":" + String(random(INT_MAX));
     auto res = runTests(r, keyPrefix);
-    Serial.printf("%d tests passed of %d total run.\n", res.passed, res.total);
+
+    Serial.printf("\n\n%s (%d passed / %d total)\n", 
+      (res.passed == res.total ? "SUCCESS": "FAILURE"), res.passed, res.total);
   }
 }
 
@@ -76,7 +81,7 @@ TestResults runTests(Redis *redis, String prefix, bool retainData)
             return r->set(k, "E") && r->expire(k, 100) && r->ttl(k) > 99;
         } },
         { "pttl", [=](Redis *r, const char* k) { 
-            return r->set(k, "E") && r->pexpire(k, 1000) && r->pttl(k) > 900;
+            return r->set(k, "E") && r->pexpire(k, 1000) && r->pttl(k) > 750; // allows up to 250ms latency between pexpire & pttl calls
         } },
         { "dlyexp", [=](Redis *r, const char* k) {
             auto sr = r->set(k, "DE");
@@ -105,6 +110,23 @@ TestResults runTests(Redis *redis, String prefix, bool retainData)
         { "hdel", [=](Redis *r, const char* k) { 
             return r->hset(k, "delete_me", k) && r->hdel(k, "delete_me") && r->hget(k, "delete_me") == NULL; 
         } },
+        { "hlen", [=](Redis *r, const char* k) {
+          auto lim = random(64) + 64;
+          auto key = k + String(":hlen");
+          
+          for (auto i = 0; i < lim; i++) {
+            r->hset(key.c_str(), (String("hlen_test__") + String(i)).c_str(), String(i).c_str());
+          }
+
+          auto verif = r->hlen(key.c_str());
+
+          // the generic cleanup doesn't take care of these
+          for (auto i = 0; i < lim; i++) {
+            r->hdel(key.c_str(), (String("hlen_test__") + String(i)).c_str());
+          }
+          
+          return verif == lim;
+        } },
         { "append", [=](Redis *r, const char* k) { 
             return r->append(k, "foo") == 3; 
         } },
@@ -113,7 +135,7 @@ TestResults runTests(Redis *redis, String prefix, bool retainData)
         } }
     };
     
-    Serial.printf("Redis::run_tests(\"%s\") starting...\n", prefix.c_str());
+    Serial.printf("\nRunning tests (key prefix: \"%s\"):\n", prefix.c_str());
 
     auto pFunc = [&prefix](const String& n) { return String(prefix + "." + n); };
 
@@ -124,14 +146,14 @@ TestResults runTests(Redis *redis, String prefix, bool retainData)
         auto tRes = kv.second(redis, pFunc(tName).c_str());
         total++;
         pass += tRes ? 1 : 0;
-        Serial.printf("  * \"%s\": %s\n", tName.c_str(), tRes ? "pass" : "FAIL");
+        Serial.printf("  %s %s\n", tRes ? "✔" : "X", tName.c_str());
     }
 
-    Serial.printf("Result: %d passed / %d total\n", pass, total);
-
     if (!retainData) {
+      Serial.printf("\nCleaning up (set retainData=true to skip)...\n");
         for (auto& kv : g_Tests) {
-            (void)redis->del(pFunc(kv.first).c_str());
+            auto rmed = redis->del(pFunc(kv.first).c_str());
+            Serial.printf("  %s %s\n", rmed ? "✔" : "X", kv.first.c_str());
         }
     }
 
